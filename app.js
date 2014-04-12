6832655ea2f63ae20b81bf2e3ca647a21d698c5c
Sam Kreter <sakfy6@mail.missouri.edu>
2014-04-12 11:54:39 -0500

    Figured out JSON

--- app.js
+++ app.js
@@ -14,12 +14,18 @@ var app = express();
 var tsession = require("temboo/core/temboosession");
 var session = new tsession.TembooSession("samkreter", "Rpicker", "1b32c6ff7d394e29885041b925f4f458");
 
-var Foursquare = require("temboo/Library/Foursquare/Venues")
-var ExploreChoreo = new Foursquare.Explore(session);
-var ExploreInput = ExploreChoreo.newInputSet();
-ExploreInput.set_AccuracyOfCoordinates(100);
-var sucessFunction = function(results) {console.og("Success!)");}
-var errorFunction = function(results) {console.log("Error!");}
+var Foursquare = require("temboo/Library/Foursquare/Venues");
+
+var exploreChoreo = new Foursquare.Explore(session);
+
+// Instantiate and populate the input set for the choreo
+var exploreInputs = exploreChoreo.newInputSet();
+
+// Set inputs
+exploreInputs.set_ClientSecret("CGF0CXUPEOMXPVLF1OVLITXQWJRETO4SI5OOAVGR0DM441EM");
+exploreInputs.set_Latitude("38.94038");
+exploreInputs.set_Longitude("-92.32774");
+exploreInputs.set_ClientID("0Y4WX42SOV1NXZOC2AGSUZAUOKQ1LVYDIK4WAMI5UK0OG3LU");
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -38,12 +44,26 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res) {
-  res.type('text/plain');
-  ExploreChoreo.execute(ExploreInput,sucessFunction,errorFunction)
-});
-app.get('/users', user.list);
+/*app.get('/', function(req, res) {
+  exploreChoreo.execute(
+    exploreInputs,
+    response = function(results){return results.get_Response();},
+    function(error){console.log(error.type); console.log(error.message)});
+     console.log(response);
+ });*/
 
+var obj;
+app.get('/',function(req,res){
+  exploreChoreo.execute(
+      exploreInputs,
+      function(results){
+      	inspectObj(JSON.parse(results.get_Response()))
+      	;},
+      function(error){console.log(error.type); console.log(error.message);});
+});
+function inspectObj(obj){
+   console.log(obj.response.groups[0].items[0].venue.name);
+};
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
