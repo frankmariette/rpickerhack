e6ba42fda296f856449dacbf4adb7305e045b5da
JoeLoser <jrlmwc@mail.misouri.edu>
2014-04-12 01:36:13 -0500

    made joe branch

---
+++ yelpTest.js
@@ -0,0 +1,18 @@
+var yelp = require("yelp").createClient({
+  consumer_key: "consumer-key",
+  consumer_secret: "consumer-secret",
+  token: "token",
+  token_secret: "token-secret"
+});
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+  console.log(error);
+  console.log(data);
+});
+
+// See http://www.yelp.com/developers/documentation/v2/business
+yelp.business("yelp-san-francisco", function(error, data) {
+  console.log(error);
+  console.log(data);
+});
